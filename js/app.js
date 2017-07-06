ccbc83cfba41da4d9a05da5b7a3975f92283b235
SamCatherman <sam.catherman@gmail.com>
2017-07-06 15:23:15 -0400

    solves scope data binding issue

--- js/app.js
+++ js/app.js
@@ -6,11 +6,24 @@ angular.module('ngAir', [])
       getAirQuality();
     });
 
-    $scope.search = "springfield";
-    // $scope.searchState = "massachusetts";
+    $scope.searchCountry = "usa";
+    $scope.searchCity = "springfield";
+    $scope.searchState = "massachusetts";
 
     let getAirQuality = () => {
-      $http.get("http://api.airvisual.com/v2/city?country=usa&state=massachusetts&city=" + $scope.search + "&key=can'tseeme")
-        .then((response) => { console.log(response)})
+      $http.get("http://api.airvisual.com/v2/city?country=" + $scope.searchCountry + "&state=" + $scope.searchState + "&city=" + $scope.searchCity + "&key=can'tseeme")
+        .then((response) => {
+          console.log(response);
+          console.log($scope.searchCountry);
+          console.log(response.data.data.current.pollution.aqius);
+        })
     }
   });
+//todo: do I need to reference each model in the scope.watch function?
+//in OMBD example, there is only one search model, for the Movie title.
+// How would I handle multiple fields, country/city/state?
+
+
+//hide API key
+
+//figure out correct expression for ng-if in air-data.html partial page
